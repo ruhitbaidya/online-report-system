f41dd6fd36153cb5369846c147d71256936b3d49
ruhitbaidya <ruhitbaidya01@gmail.com>
2025-07-04 01:02:28 +0600

    backend image work not finsih

--- server/src/app/utils/fileUpload.ts
+++ server/src/app/utils/fileUpload.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './upload');
+    cb(null, "./upload");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
