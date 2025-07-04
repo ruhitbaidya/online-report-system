cef562826c0aae7305d4d8e21da465db5c222d98
ruhitbaidya <ruhitbaidya01@gmail.com>
2025-07-04 14:06:13 +0600

    image upload finish

--- server/src/app/config/configs.ts
+++ server/src/app/config/configs.ts
@@ -5,4 +5,7 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export const configs = {
   db_url: process.env.DB_URL,
   port: process.env.PORT,
+  coud_name : process.env.CLOUDINARY_NAME,
+  coud_Key : process.env.CLOUDINARY_KEY,
+  coud_secrate : process.env.CLOUDINARY_SECRATE
 };
--- server/src/app/modules/user/user.controler.ts
+++ server/src/app/modules/user/user.controler.ts
@@ -1,9 +1,11 @@
 import catchAsyncFun from "../../utils/catchAsyncFunc";
 import sendReponse from "../../utils/sendResponse";
+import { uploadImage } from "../../utils/uploadImageCloudinary";
 import { userServices } from "./user.services";
 
 const createUser = catchAsyncFun(async (req, res) => {
-  const resData = req.body;
+  const resData = req.file?.path;
+  await uploadImage("ruhit", req.file?.path as string) 
   console.log(resData);
   // const result = await userServices.createUser(resData)
   // return sendReponse(res, {success : true, message : "User Create Successfull", status : 200, data : result})
