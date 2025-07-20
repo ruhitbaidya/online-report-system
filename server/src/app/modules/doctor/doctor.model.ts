0a589ca758e0cc478671f5586ec6c9d16ee17fa2
ruhitbaidya <ruhitbaidya01@gmail.com>
2025-07-20 23:55:48 +0600

    this post had done some work due

--- server/src/app/modules/doctor/doctor.model.ts
+++ server/src/app/modules/doctor/doctor.model.ts
@@ -44,4 +44,16 @@ const doctorsSchema = new Schema<TDoctors>(
   { timestamps: true }
 );
 
+doctorsSchema.pre("save", async function (next) {
+  try {
+    const matcher = await doctorsModel.findOne({ email: this.email });
+    if (matcher) {
+      return next(new Error("This email already exists."));
+    }
+    next();
+  } catch (err: any) {
+    next(err);
+  }
+});
+
 export const doctorsModel = model<TDoctors>("doctors", doctorsSchema);
